8a2a365aebc0c2d82c90d51ccecedc8280531843
Edécio Fernando <edeciofernando@gmail.com>
2021-08-21 14:43:48 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [nome, setNome] = useState("");
+  const [pacientes, setPacientes] = useState([]);
+
+  const handleNome = e => {
+    setNome(e.target.value);
+  }
+
+  const handleAdd = e => {
+    e.preventDefault();
+
+    if (nome.trim().length <= 2) {
+      alert("Informe o nome completo do paciente");
+      return;
+    }
+
+    // spread operator (junta os pacientes da lista, com o novo paciente)
+    setPacientes([...pacientes, nome]);
+
+    // limpa o nome (ou seja, o campo do form)
+    setNome("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row mt-3">
+        <div className="col-sm-4">
+          <img src="dentista.jpg" alt="Dentista" className="img-fluid" />
+        </div>
+        <div className="col-sm-8">
+          <h3>Centro Odontológico Avenida</h3>
+          <form onSubmit={handleAdd}>
+            <div className="form-group">
+              <label htmlFor="nome">Nome do Paciente:</label>
+              <input type="text" className="form-control" id="nome"
+                     value={nome}
+                     onChange={handleNome} />
+            </div>
+            <input
+              type="submit"
+              className="btn btn-primary"
+              value="Adicionar"
+            />
+          </form>
+          <h4 className="mt-3">Lista de Pacientes na Fila:</h4>
+          <ol className="text-primary font-weight-bold">
+            {
+              pacientes.map(paciente => {
+                return <li key={paciente}>{paciente}</li>
+              })  
+            }
+          </ol>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
